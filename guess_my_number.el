ed2531d94c217e2b264ede6479aa6d1e150f505d
Javier Aguirre <contacto@javaguirre.net>
2014-08-23 19:17:48 +0200

    Guess my number example, game example till walk action

---
+++ guess_my_number.el
@@ -0,0 +1,17 @@
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over ()
+  (let ((*small* 1)
+        (*big* 100))
+  (guess-my-number))
+
+(start-over)
